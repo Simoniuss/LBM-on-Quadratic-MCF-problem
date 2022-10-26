1cf2d78dab4f0cf422d2eac33e9ae4720bbb0041
Simone <59289872+Simoniuss@users.noreply.github.com>
2022-10-26 17:18:23 +0200

    Add mat converter script

--- src/qcnd/generateMCF.sh
+++ src/qcnd/generateMCF.sh
@@ -5,6 +5,20 @@ if [ $# != 6 ]; then
     exit 1
 fi
 
-./pargen $1 $2 $3 $4 $5 $6
-./netgen/netgen < netgen-$1-$2-$3-$4-$5-$6.par > netgen-$1-$2-$3-$4-$5-$6.dmx
-./qfcgen netgen-$1-$2-$3-$4-$5-$6.dmx
+./pargen $1 $2 $3 $4 $5 $6 &&
+echo "par generated!" &&
+./netgen/netgen < netgen-$1-$2-$3-$4-$5-$6.par > netgen-$1-$2-$3-$4-$5-$6.dmx &&
+echo "dmx generated!" &&
+./qfcgen netgen-$1-$2-$3-$4-$5-$6.dmx &&
+echo "qfc generated!" &&
+python3 convertertomat.py netgen-$1-$2-$3-$4-$5-$6 &&
+echo "mat generated!"
+
+
+if [ -d "../data" ];
+then
+    mv netgen-$1-$2-$3-$4-$5-$6.* ../data
+else
+    mkdir ../data
+    mv netgen-$1-$2-$3-$4-$5-$6.* ../data
+fi

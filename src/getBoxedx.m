cccadf5ed78796f0dc8383845f6b066035d3053e
Simone <59289872+Simoniuss@users.noreply.github.com>
2022-11-02 14:23:22 +0100

    Bug fixing

--- src/getBoxedx.m
+++ src/getBoxedx.m
@@ -8,13 +8,13 @@ x = zeros(n);
 ET = E';
 for i = 1:n
     if Q(i) == 0
-        if mu'*b < q(i)'*u(i) + mu'*( E(:,i)'*u(i) - b)
+        if mu'*b < q(i)*u(i) + mu'*( E(:,i)'*u(i) - b)
             x(i) = 0;
         else
             x(i) = u(i);
         end
     else
-        x(i) = (-q(i)' - ET(i,:)*mu)/(2*Q(i));
+        x(i) = (-q(i) - ET(i,:)*mu)/(2*Q(i));
         x(i) = max(0, min(x(i), u(i)));
     end
 end

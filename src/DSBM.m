c390c6b147ee8777c55ac835cb89b0993962bcc6
Simone <59289872+Simoniuss@users.noreply.github.com>
2022-12-15 15:33:42 +0100

    Another implementation (again)

--- src/DSBM.m
+++ src/DSBM.m
@@ -1,38 +1,20 @@
-function [mu, exitflag] = DSBM(B_z, B_alpha, l, mu)
-% Solve the DP for the LBM finding the weights theta used to build a
-% a feasible solution. This function solves the following problem:
-%       theta = arg min { 0.5*norm(sum(z^b*theta^b))^2 + sum((l+alpha^b)*theta), b \in B}
-% B is the bundle which contains subgradients z and alpha, and l is the
-% level parameter.
-%
+function [mu, exitflag, theta, rho] = DSBM(B_z, B_alpha, l, mu)
 
-% Number of pairs in the bundle
-bundleSize = size(B_z,2);
 muSize = size(mu,1);
-
-
-% mu
 muVar = sdpvar(muSize,1);
 vVar = sdpvar(1,1);
 
-
-Constraints = [ l >= vVar, vVar * ones(bundleSize,1) >= B_z'*muVar - B_alpha'];
-
-%Constraints = B_z'*(muVar) - B_alpha <= l;
+Constraints = [ (l >= vVar):'rho', (vVar >= B_z'*muVar - B_alpha'):'theta'];
 Objective = vVar + 0.5*norm(muVar - mu)^2;
 
 %options = sdpsettings('verbose', 0, 'solver','quadprog');
 options = sdpsettings('verbose', 0, 'solver','mosek');
 %options = sdpsettings('verbose', 0);
 
-%if bundleSize == 1
-%    savecplexlp(Constraints, Objective, 'DSBM1');
-%elseif bundleSize == 2
-%    savecplexlp(Constraints, Objective, 'thetaDPData2');
-%end
-
 sol = optimize(Constraints,Objective, options);
 mu = value(muVar);
+theta = dual(Constraint('theta'));
+rho = dual(Constraint('rho'));
 exitflag = sol.problem;
 
 end
